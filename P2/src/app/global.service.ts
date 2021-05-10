fe88eb10c5ddf269f2b3eafdb487f12f37e066aa
Eva <100383379@alumnos.uc3m.es>
2021-05-10 12:18:12 +0200

    fallo edicion primera tarjeta solved

--- P2/src/app/global.service.ts
+++ P2/src/app/global.service.ts
@@ -9,12 +9,14 @@ export class GlobalService {
   constructor() { }
 
   public datos: contacto[] = [
-    {id: 0, nombre: 'Tarjeta TTP', serial_number: '0061274879', caducidad: new Date(2024, 2, 29), tipo: 1, habitual: true},
-    {id: 1, nombre: 'Tarjeta Multi',serial_number: '1061295300', caducidad: new Date(2030, 7, 12), tipo: 2, habitual: false},
-    {id: 2, nombre: 'Tarjeta Renfe&Tú',  serial_number: '8064374879',caducidad: new Date(2025, 1, 25),tipo: 3, habitual:false},
+    {id: 1, nombre: 'Tarjeta TTP', serial_number: '0061274879', caducidad: new Date(2024, 2, 29), tipo: 1, habitual: true},
+    {id: 2, nombre: 'Tarjeta Multi',serial_number: '1061295300', caducidad: new Date(2030, 7, 12), tipo: 2, habitual: false},
+    {id: 3, nombre: 'Tarjeta Renfe&Tú',  serial_number: '8064374879',caducidad: new Date(2025, 1, 25),tipo: 3, habitual:false},
 
   ];
 
+  
+  
 
   public readcontacto(id:number):contacto
   {
@@ -23,9 +25,10 @@ export class GlobalService {
 
   public eliminar(id)
   {
-    console.log('Eliminando:'+id);
-    this.datos.splice(id,1);
-    let i=0;
+    console.log('Eliminando:' + id);
+    console.log(this.datos);
+    this.datos.splice(id-1,1);
+    let i=1;
     for(let dato of this.datos)
     {
       dato.id=i;
